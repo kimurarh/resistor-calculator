4ade4fca37f02aee07df5e72f12657717e838038
Rafael Kimura <rafaelkimura94@gmail.com>
2020-06-10 17:54:02 -0300

    minor changes to color classes

--- scripts/index.js
+++ scripts/index.js
@@ -20,12 +20,16 @@ function calculateResistor() {
         multi = "k";
     }
 
+    // round number to 2 decimal places
+    digit = Math.round(digit * 100) / 100;
+
     var tolTemp = ", " + getIndex("colorFive", "parameter5") + ", " + getIndex("colorSix", "parameter6");
 
     document.getElementById("resistorValue").innerHTML = digit + multi + " Ohms" + tolTemp; 
 }
 
 function changeColorDigit(formId, parameterId) {
+    var txtColor = "";
     switch (getIndex(formId, parameterId)) {
         case '0': 
             color = '#000000'; /* Black */
@@ -38,12 +42,15 @@ function changeColorDigit(formId, parameterId) {
             break;
         case '3':
             color = '#ffa500'; /* Orange */
+            txtColor = '#000000';
             break;
         case '4': 
             color = '#ffff00'; /* Yellow */
+            txtColor = '#000000';
             break;
         case '5':
             color = '#00b250'; /* Green */
+            txtColor = '#000000';
             break;
         case '6':
             color = '#0071c2'; /* Blue */
@@ -56,22 +63,31 @@ function changeColorDigit(formId, parameterId) {
             break;
         case '9':
             color = '#ffffff'; /* White */
+            txtColor = '#000000';
             break;
         default:
             color = '#fff2ca';  /* resistor body color*/
     }
     document.getElementById(parameterId).style.backgroundColor = color;
     document.getElementById(formId).style.backgroundColor = color;
+    if(txtColor != "") {
+        document.getElementById(formId).style.color = txtColor;
+    } else {
+        document.getElementById(formId).style.color = '#ffffff';
+    }
     calculateResistor();
 }
 
     function changeColorMultiplier(formId, parameterId) {
+    var txtColor = "";
     switch (getIndex(formId, parameterId)) {
         case '0.01': 
             color = '#dadada'; /* Silver */
+            txtColor = '#000000';
             break;
         case '0.1':
             color = '#e2d600'; /* Gold */
+            txtColor = '#000000';
             break;
         case '1': 
             color = '#000000'; /* Black */
@@ -84,12 +100,15 @@ function changeColorDigit(formId, parameterId) {
             break;
         case '1000':
             color = '#ffa500'; /* Orange */
+            txtColor = '#000000';
             break;
         case '10000': 
             color = '#ffff00'; /* Yellow */
+            txtColor = '#000000';
             break;
         case '100000':
             color = '#00b250'; /* Green */
+            txtColor = '#000000';
             break;
         case '1000000':
             color = '#0071c2'; /* Blue */
@@ -102,16 +121,24 @@ function changeColorDigit(formId, parameterId) {
     }
     document.getElementById(parameterId).style.backgroundColor = color;
     document.getElementById(formId).style.backgroundColor = color;
+    if(txtColor != "") {
+        document.getElementById(formId).style.color = txtColor;
+    } else {
+        document.getElementById(formId).style.color = '#ffffff';
+    }
     calculateResistor();
 }
 
 function changeColorTolerance(formId, parameterId) {
+    var txtColor = "";
     switch (getIndex(formId, parameterId)) {
         case '10%': 
             color = '#dadada'; /* Silver */
+            txtColor = '#000000';
             break;
         case '5%':
             color = '#e2d600'; /* Gold */
+            txtColor = '#000000';
             break;
         case '1%':
             color = '#a52c2c'; /* Brown */
@@ -121,6 +148,7 @@ function changeColorTolerance(formId, parameterId) {
             break;
         case '0.5%':
             color = '#00b250'; /* Green */
+            txtColor = '#000000';
             break;
         case '0.25%':
             color = '#0071c2'; /* Blue */
@@ -136,10 +164,16 @@ function changeColorTolerance(formId, parameterId) {
     }
     document.getElementById(parameterId).style.backgroundColor = color;
     document.getElementById(formId).style.backgroundColor = color;
+    if(txtColor != "") {
+        document.getElementById(formId).style.color = txtColor;
+    } else {
+        document.getElementById(formId).style.color = '#ffffff';
+    }
     calculateResistor();
 }
 
 function changeColorTemperature(formId, parameterId) {
+    var txtColor = ""
     switch (getIndex(formId, parameterId)) {
         case '100ppm':
             color = '#a52c2c'; /* Brown */
@@ -149,15 +183,22 @@ function changeColorTemperature(formId, parameterId) {
             break;
         case '15ppm':
             color = '#ffa500'; /* Orange */
+            txtColor = '#000000';
             break;
         case '25ppm': 
             color = '#ffff00'; /* Yellow */
+            txtColor = '#000000';
             break;
         default:
             color = '#fff2ca'; /* resistor body color*/
     }
     document.getElementById(parameterId).style.backgroundColor = color;
     document.getElementById(formId).style.backgroundColor = color;
+    if(txtColor != "") {
+        document.getElementById(formId).style.color = txtColor;
+    } else {
+        document.getElementById(formId).style.color = '#ffffff';
+    }
     calculateResistor();
 }
 
